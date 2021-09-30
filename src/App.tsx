b185de75e205724ca346fdfad00543a2ee937e7e
545047874 <39019798+545047874@users.noreply.github.com>
2021-09-30 17:24:16 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-// import './test.less'
+import './stories/style/index'
 import {Button} from './stories/Button/index'
 
 function App() {
   return (
     <div className="App">
       <Button label="asd"></Button>
+      <div className="a">111</div>
     </div>
   );
 }
